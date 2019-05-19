824ff8afe5e296c0617495a430a67c4f9a177371
efrenmartinez <developer.emartinez@gmail.com>
2019-05-18 22:14:09 -0500

    [FEATURE]CHANGE DE URL OF THE API

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   env: {
-    API_URL: 'https://pokeapi.co/api/v2/'
+    API_URL: 'https://gist.githubusercontent.com/efrenmartinez/be8a8e3bd118d3105125f5c0bf82a54c/raw/763b3d6c856010555e92adb889b6b342dfe51063'
   },
   css: ['bulma/css/bulma.css']
 }
